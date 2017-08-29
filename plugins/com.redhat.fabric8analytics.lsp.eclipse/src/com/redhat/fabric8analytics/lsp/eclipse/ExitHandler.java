4e76647b4a7ed203e61ff442e1f1c8a9a49aa236
Geetika Batra <gbatra@Geetikas-MacBook-Pro.local>
2017-08-29 13:01:14 +0530

    Adds support for Multiple manifest files

--- plugins/com.redhat.fabric8analytics.lsp.eclipse/src/com/redhat/fabric8analytics/lsp/eclipse/ExitHandler.java
+++ plugins/com.redhat.fabric8analytics.lsp.eclipse/src/com/redhat/fabric8analytics/lsp/eclipse/ExitHandler.java
@@ -2,6 +2,11 @@ package com.redhat.fabric8analytics.lsp.eclipse;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -13,24 +18,25 @@ import org.apache.http.impl.client.HttpClients;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.swt.*;
 
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.progress.UIJob;
 import org.json.JSONObject;
 import com.redhat.fabric8analytics.lsp.eclipse.CustomView;
@@ -40,76 +46,105 @@ import com.redhat.fabric8analytics.lsp.eclipse.TokenCheck;
 public class ExitHandler extends AbstractHandler {
 	private String RECOMMENDER_API_TOKEN = "";
 	static  String jobId;
+	public static List<IFile> findAllProjectFiles(IContainer container) throws CoreException {
+		IResource[] members = container.members();
+		List<IFile> list = new ArrayList<>();
+
+		for (IResource member : members) {
+			if (member instanceof IContainer) {
+				IContainer c = (IContainer) member;
+				list.addAll(findAllProjectFiles(c));
+			} else if (member instanceof IFile) {
+				list.add((IFile) member);
+			}
+		}
+		return list;
+	}
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		//		final String jobId;
 		Shell mapComposite = HandlerUtil.getActiveWorkbenchWindow(event).getShell();
 
 		try {
+			IWorkspace workspace = ResourcesPlugin.getWorkspace();
+			IWorkspaceRoot root = workspace.getRoot();
+			IProject[] projects = root.getProjects();
+			List<IFile> projectfiles = new ArrayList<>();
+			for (IProject project : projects) {
 
-			ISelectionService service = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
-			 
-			ISelection selection = service.getSelection();
-
-			if (selection instanceof IStructuredSelection)
-			{
-				Object selected = ((IStructuredSelection)selection).getFirstElement();
-
-				IFile file = (IFile)Platform.getAdapterManager().getAdapter(selected, IFile.class);
-				IPath filePath = file.getLocation();
-
-
-				String token = TokenCheck.getToken();
-				while(token==null || token.isEmpty()) {					
-					TokenCheck.checkToken();	
-					token = TokenCheck.getToken();
+				projectfiles =findAllProjectFiles(project);
+			}
+			Map<String, FileBody> manifestLocationMap = new HashMap<>();
+			for(IFile temp_file : projectfiles) {
+				String temp_uri = temp_file.toString();
+				String[] split_url = temp_uri.split("/");
+				if(temp_uri.contains("pom.xml")) {
+					System.out.println(temp_uri);
 				}
-				if(!RECOMMENDER_API_TOKEN.equals("Bearer " + token)) {
-					RECOMMENDER_API_TOKEN = "Bearer "+ token;
-				}
-
-				
-				
-
-			    
-				String viewId = "de.vogella.rcp.commands.first.commands.Exit";
-				IViewPart temp=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(viewId); 
-				CloseableHttpClient client = HttpClients.createDefault();
-				HttpPost post = new HttpPost("https://recommender.api.openshift.io/api/v1/stack-analyses-v2/");		
-				post.addHeader("Authorization" , RECOMMENDER_API_TOKEN);
-				FileBody fileBody = new FileBody(new File(filePath.toString()));
-				MultipartEntityBuilder builder = MultipartEntityBuilder.create()
-						.setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
-						.addPart("manifest[]", fileBody)
-						.addTextBody("filePath[]", filePath.toString());
-
-				HttpEntity multipart = builder.build();
-				post.setEntity(multipart);
-				HttpResponse response = client.execute(post);
-				JSONObject jsonObj = Utils.jsonObj(response);
-				if (response.getStatusLine().getStatusCode()==200) {
-					setJobId(jsonObj.getString("id"));
-					WorkerThread workerThread = new WorkerThread((CustomView)temp);
-					workerThread.go();
+				if(split_url.length>3) {
+					if(split_url[2].equals("pom.xml") || split_url[3].equals("pom.xml")) {
+						if(!split_url[2].equals("bin"))
+						{
+							FileBody fileBody = new FileBody(new File(temp_file.getLocation().toString()));
+							manifestLocationMap.put(temp_uri, fileBody);
+						}
+					}
 
 				}
 				else {
-					new UIJob(PlatformUI.getWorkbench().getDisplay(), jsonObj.toString()) {
-						@Override
-						public IStatus runInUIThread(IProgressMonitor monitor) {
-							MessageDialog.openError(getDisplay().getActiveShell(), "ERROR", jsonObj.toString());
-							return Status.OK_STATUS;
-						}
-					}.schedule();
-					return mapComposite;
+					if(split_url[2].equals("pom.xml")) {
+
+						FileBody fileBody = new FileBody(new File(temp_file.getLocation().toString()));
+						manifestLocationMap.put(temp_uri, fileBody);
+
+					}
 				}
 
 
 			}
+			String token = TokenCheck.getToken();
+			while(token==null || token.isEmpty()) {					
+				TokenCheck.checkToken();	
+				token = TokenCheck.getToken();
+			}
+			if(!RECOMMENDER_API_TOKEN.equals("Bearer " + token)) {
+				RECOMMENDER_API_TOKEN = "Bearer "+ token;
+			}
+			String viewId = "de.vogella.rcp.commands.first.commands.Exit";
+			IViewPart temp=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(viewId); 
+			CloseableHttpClient client = HttpClients.createDefault();
+			HttpPost post = new HttpPost("https://recommender.api.openshift.io/api/v1/stack-analyses/");		
+			post.addHeader("Authorization" , RECOMMENDER_API_TOKEN);
+			MultipartEntityBuilder builder = MultipartEntityBuilder.create()
+					.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+			for (Map.Entry<String, FileBody>  entry : manifestLocationMap.entrySet())
+			{
+				builder.addPart("manifest[]", entry.getValue())
+				.addTextBody("filePath[]", entry.getKey());
+			}
 
 
+			HttpEntity multipart = builder.build();
+			post.setEntity(multipart);
+			HttpResponse response = client.execute(post);
+			JSONObject jsonObj = Utils.jsonObj(response);
+			if (response.getStatusLine().getStatusCode()==200) {
+				setJobId(jsonObj.getString("id"));
+				WorkerThread workerThread = new WorkerThread((CustomView)temp);
+				workerThread.go();
 
-		} catch (SWTError | IOException | PartInitException e) {
+			}
+			else {
+				new UIJob(PlatformUI.getWorkbench().getDisplay(), jsonObj.toString()) {
+					@Override
+					public IStatus runInUIThread(IProgressMonitor monitor) {
+						MessageDialog.openError(getDisplay().getActiveShell(), "ERROR", jsonObj.toString());
+						return Status.OK_STATUS;
+					}
+				}.schedule();
+				return mapComposite;
+			}
+
+		} catch (SWTError | IOException | CoreException e) {
 			e.printStackTrace();
 		}
 
--- plugins/com.redhat.fabric8analytics.lsp.eclipse/src/com/redhat/fabric8analytics/lsp/eclipse/Utils.java
+++ plugins/com.redhat.fabric8analytics.lsp.eclipse/src/com/redhat/fabric8analytics/lsp/eclipse/Utils.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
@@ -31,8 +32,7 @@ public class Utils {
 
 	public static int checkStackProgress(String jobId) throws ClientProtocolException, IOException, PartInitException{
 		CloseableHttpClient client = HttpClients.createDefault();
-		String recommendUrl = "https://recommender.api.openshift.io/api/v1/stack-analyses-v2/";
-//		int getResponseCode ;
+		String recommendUrl = "https://recommender.api.openshift.io/api/v1/stack-analyses/";
 		String RECOMMENDER_API_TOKEN = "Bearer ";
 		String token = TokenCheck.getToken();
 		while(token==null || token.isEmpty()) {					
@@ -45,23 +45,11 @@ public class Utils {
 		HttpGet get = new HttpGet(recommendUrl + jobId);
 		get.addHeader("Authorization" , RECOMMENDER_API_TOKEN);
 		HttpResponse getResponse = client.execute(get);
-		//		 String viewId = "de.vogella.rcp.commands.first.commands.Exit";
 		return getResponse.getStatusLine().getStatusCode();
-		//		IViewPart temp=PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(viewId); 
-		//			Browser browser =  ((CustomView) temp).getBrowser();
-		//		browser = ((CustomView) temp).getBrowser();
-		//		if(getResponseStatus==200) {
-		//			browser.setUrl("http://ops-portal-v2-ops-portal-ide.dev.rdu2c.fabric8.io/#/analyze/" + jobId);
-		//			return true;
-		//		}
-		//		else if(getResponseStatus==202) {
-		//			//				browser.setUrl("file:///Users/gbatra/fabric8/fabric8-analytics-devstudio-plugin/plugins/com.redhat.fabric8analytics.lsp.eclipse/templates/index.html");
-		//			browser.setUrl("http://google.com");
-		//			return false;
-		//		}
-		//		throw new ClientProtocolException();
+		
 	}
+	 
 
-
+	
 
 }
