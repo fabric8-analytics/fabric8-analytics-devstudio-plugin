fde25e42a496c9e78b8ba4be40a1948e8700ce29
Geetika Batra <gbatra@redhat.com>
2017-10-11 16:00:08 +0530

    Removed worker thread module and used eclipse jobs

---
+++ plugins/com.redhat.fabric8analytics.lsp.eclipse.ui/src/com/redhat/fabric8analytics/lsp/eclipse/ui/AnalysesJobHandler.java
@@ -0,0 +1,63 @@
+package com.redhat.fabric8analytics.lsp.eclipse.ui;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IViewPart;
+
+import com.redhat.fabric8analytics.lsp.eclipse.core.RecommenderAPIException;
+import com.redhat.fabric8analytics.lsp.eclipse.core.RecommenderAPIProvider;
+import com.redhat.fabric8analytics.lsp.eclipse.ui.ExitHandler;
+
+public class AnalysesJobHandler extends Job{
+			
+		private static final int   TIMER_INTERVAL = 10000;
+		private static String jobId = null;
+		public AnalysesJobHandler(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+		protected IStatus run(IProgressMonitor monitor) {
+			URL url;
+			try {
+				url = new URL("platform:/plugin/com.redhat.fabric8analytics.lsp.eclipse.ui/templates/index.html");
+				url = FileLocator.toFileURL(url);
+				IViewPart mainView = ExitHandler.getView();
+				jobId = ExitHandler.getJobId();
+				((StackAnalysesView) mainView).updatebrowserUrl(url.toString());
+				setTimerAnalyses();
+				syncWithUi(mainView);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return Status.OK_STATUS;
+		}
+		private void setTimerAnalyses() {
+			try {
+				while(!RecommenderAPIProvider.getInstance().analysesFinished(jobId, TokenCheck.get().getToken())){
+					Thread.sleep(TIMER_INTERVAL);
+				}
+			} catch (RecommenderAPIException | InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		private void syncWithUi(IViewPart mainView) {
+			Display.getDefault().asyncExec(new Runnable() {
+				public void run() {
+					((StackAnalysesView) mainView).updatebrowserUrl(RecommenderAPIProvider.getInstance().getAnalysesURL(jobId, TokenCheck.get().getToken()));
+				}
+			});
+
+		}
+}
