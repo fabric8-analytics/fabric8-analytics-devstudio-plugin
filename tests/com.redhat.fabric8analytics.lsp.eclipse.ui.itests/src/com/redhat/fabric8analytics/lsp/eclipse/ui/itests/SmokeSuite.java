d7520eb76255d8200abdef90c9169b9a92831b52
Vojtěch Průša <prusa.vojtech@gmail.com>
2017-12-14 13:25:11 +0100

    Added copyrights to tests (#97)

--- tests/com.redhat.fabric8analytics.lsp.eclipse.ui.itests/src/com/redhat/fabric8analytics/lsp/eclipse/ui/itests/SmokeSuite.java
+++ tests/com.redhat.fabric8analytics.lsp.eclipse.ui.itests/src/com/redhat/fabric8analytics/lsp/eclipse/ui/itests/SmokeSuite.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2017 Red Hat, Inc.
- * Distributed under license by Red Hat, Inc. All rights reserved.
- * This program is made available under the terms of the
- * Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2017 Red Hat Inc..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributor:
- *     Red Hat, Inc. - initial API and implementation
- ******************************************************************************/
+ * Contributors:
+ *     Red Hat Incorporated - initial API and implementation
+ *******************************************************************************/
 package com.redhat.fabric8analytics.lsp.eclipse.ui.itests;
 
 import org.eclipse.reddeer.junit.runner.RedDeerSuite;
