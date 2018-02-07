8a3866ed78c2c83e54d938f1d5cbbd3237254cc3
Geetika Batra <geetika791@gmail.com>
2018-02-07 15:01:42 +0530

    Removing bash script

--- plugins/com.redhat.fabric8analytics.lsp.eclipse.ui/scripts/lsp-server.sh
+++
@@ -1,9 +0,0 @@
-rm -rf ca-lsp-server*
-
-wget https://github.com/invincibleJai/sampleBootstrapTemplate/releases/download/0.0.6/ca-lsp-server.tar
-
-archive=ca-lsp-server-0.0.6-SNAPSHOT.tar
-mkdir ${archive%.tar*} 
-tar --extract --file=${archive} --strip-components=1 --directory=${archive%.tar*}
-
-chmod +x ${archive%.tar*}/server.js
